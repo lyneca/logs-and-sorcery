f2d4b08adce9f8b74ad8212476490d74fa321637
Luke Tuthill <luke@warpfrog.com>
2026-07-16 18:42:17 +1000

    Update detection

--- parse.js
+++ parse.js
@@ -3067,6 +3067,9 @@ function matchSystemInfo(line) {
   if (match(line, /^Mono path\[0\] = '.+\\steamapps\\common.+/i, () => {
     game.system.platform ??= "Steam";
   })) return true;
+  if (match(line, /^Validation result: False/, () => {
+    game.system.platform = "pirated";
+  })) return true;
   if (match(line, /^Mono path\[0\] = '.+(Downloads|Desktop).*\\steamapps\\common.+/i, () => {
     game.system.platform = "pirated";
   })) return true;
